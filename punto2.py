70d7be5b071544c49c72049fe53eafb1ce243f65
BENAVIDES PACHECO DIEGO <dbenavid5@cuc.edu.co>
2022-03-02 19:59:36 -0500

    Segundo punto dev

---
+++ punto2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 19:51:29 2022
+
+@author: dbenavid5
+"""
+
+inversion1 = int(input('Ingrese el valor de la primera inversion: \n'))
+inversion2 = int(input('Ingrese el valor de la segunda inversion: \n'))
+inversion3 = int(input('Ingrese el valor de la tercera inversion: \n'))
+valorTotalInv = inversion1 + inversion2 + inversion3;
+print(f"El valor total de la inversion es: ${valorTotalInv:,}\n")
+pi1 = (inversion1/valorTotalInv) * 100
+pi2 = (inversion2/valorTotalInv) * 100
+pi3 = (inversion3/valorTotalInv) * 100
+print(f"El inversor 1 invirtio ${valorTotalInv:,} lo cual es el {pi1}% de la inversion\n")
+print(f"El inversor 2 invirtio ${valorTotalInv:,} lo cual es el {pi2}% de la inversion\n")
+print(f"El inversor 3 invirtio ${valorTotalInv:,} lo cual es el {pi3}% de la inversion\n")

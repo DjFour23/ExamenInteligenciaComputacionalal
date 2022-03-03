e7f42c8b020e3c7879869ae82999e4c34b43b310
BENAVIDES PACHECO DIEGO <dbenavid5@cuc.edu.co>
2022-03-02 19:51:00 -0500

    Primer punto dev

--- punto1.py
+++ punto1.py
@@ -5,3 +5,14 @@ Created on Wed Mar  2 19:47:25 2022
 @author: dbenavid5
 """
 
+valorDonacion = int(input('Ingrese el valor de la donacion: \n'))
+administracion = valorDonacion * 0.35;
+sistemas = administracion * 0.25;
+telecomunicaciones = sistemas *0.1;
+contabilidad = valorDonacion - administracion - sistemas - telecomunicaciones;
+
+print(f"Del valor de la donacion: ${valorDonacion:,}\n")
+print(f"Telecomunicaciones obtuvo: ${telecomunicaciones:,}\n")
+print(f"Sistemas obtuvo: ${sistemas:,}\n")
+print(f"Administración obtuvo: ${administracion:,}\n")
+print(f"Contabilidad obtuvo: ${contabilidad:,}\n")
